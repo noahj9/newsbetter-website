41d5147bb4f2c3e930231d67aa9977390e7d26e4
noahj9 <109239448+noahj9@users.noreply.github.com>
2023-09-14 12:41:01 -0400

    more page setup

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 import logo from '../public/logo.png';
+import app from '../public/app.png';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <meta name="description" content="Your app description" />
       </Head>
 
-      <nav className="bg-purple-400 p-6 text-white flex items-center justify-between mx-10">
+      <nav className="fixed w-full bg-purple-400 p-6 text-white flex items-center justify-between top-0 shadow-md z-10">
           <Image src={logo} className="w-1/6" alt="logo" />
           <ul className="flex space-x-6 text-xl justify-center">
             <li>
@@ -30,14 +31,20 @@ export default function Home() {
         </button>
       </nav>
 
-      <section id="about" className="p-8 w-3/4 items-center mt-20 text-justify">
-        <h1 className="text-4xl font-bold">AI Enabled Newsletter Discovery and Analytics.</h1>
-        <p className="mt-4 text-lg w-3/4 text-justify">
-        Optimize your reading time and take your learning to the next level. Understand more about your current interests and related topics that others are reading about. Newsbetter innately changes the way you read your newsletters. 
-        </p>
-        <button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 px-4 py-2 rounded-full mt-5">
-          Request Access!
-        </button>
+      <section id="about" className="p-8 w-full items-center mt-5 text-justify flex">
+        <div className="">
+          <h1 className="text-4xl font-bold">AI Enabled Newsletter Discovery and Analytics.</h1>
+          <p className="mt-4 text-lg w-3/4 text-justify">
+          Optimize your reading time and take your learning to the next level. Understand more about your current interests and related topics that others are reading about. Newsbetter innately changes the way you read your newsletters. 
+          </p>
+          <button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 px-4 py-2 rounded-full mt-5">
+            Request Access!
+          </button>
+        </div>
+        <div className="">
+          <Image src={app} className="scale-75" alt="appDemoImage" />
+        </div>
+
       </section>
 
       <section id="team" className="p-8">
